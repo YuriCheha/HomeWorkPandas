8f1a902525d504976acfb3f406ca5b0f502e4aa4
ChehaYuri <sacra1364069@gmail.com>
2023-04-09 13:17:25 +0300

    задание 5

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ df = pd.read_csv(r'C:\Users\user\Downloads\DATA_Pandas.csv')
 df_stats = df.describe()
 df_stats = df_stats.transpose()
 print(df.dtypes)
-new_df = pd.DataFrame({
+new_df1 = pd.DataFrame({
     'count': df_stats['count'],
     'mean': df_stats['mean'],
     'std': df_stats['std'],
@@ -13,7 +13,7 @@ new_df = pd.DataFrame({
     '75%': df_stats['75%'],
     'max': df_stats['max']
 })
-new_df.to_json('new_dataframe.json', orient='table')
+new_df1.to_json('new_dataframe.json', orient='table')
 
 aver_val = df['Temperature (K)'].mean()
 df['delta_T'] = abs(df['Temperature (K)'] - aver_val)
@@ -39,5 +39,21 @@ new_df2 = pd.DataFrame({
     'sp_class_var': df.groupby('Star type')['Luminosity(L/Lo)'].var()
 })
 new_df2.to_xml('new_df2.xml')
+
+
+df['temperature_C'] = df['Temperature (K)']  - 273.15
+new_df = pd.DataFrame({
+    'Temperature' : df['Temperature (K)'],
+    'temperature_C' : df['temperature_C']
+})
+result_df1 = pd.concat([new_df1, new_df2], axis=1)
+new_df2.join(new_df1)
+if new_df2.isnull().values.any():
+    print('Датафрейм содержит NaN значение')
+else:
+    print('Датафрейм не содержит NaN значение')
+new_df2.interpolate()
+test_data["A"] = new_df2['Temperature (K)'].fillna(test_data['Temperature (K)'].mean())
+t = new_df2.dropna(axis=0)
 
 
